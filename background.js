0cf9309e042f90236b61a39326c5f8a60fbb629d
Markus Pettersson <markus.pettersson1998@gmail.com>
2019-11-06 20:04:44 +0100

    Okay, vi är klara

--- background.js
+++ background.js
@@ -1,10 +1,40 @@
-// background.js
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // Send a message to the active tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-    });
-  });
+// A generic onclick callback function.
+function genericOnClick(info, tab) {
+  console.log("item " + info.menuItemId + " was clicked");
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+}
+
+const title = "ta mig till katt";
+const context = "editable";
+
+function sendSearch(selectedText) {
+  const catSearch = "https://www.youtube.com/results?search_query=cat+animal+" + selectedText;
+  chrome.tabs.create({ url: catSearch });
+}
+
+chrome.contextMenus.create({
+  title: title,
+  contexts: ['selection'],
+  onclick: function(info, tab) {
+    console.log(`background.js: selected text: ${info.selectionText}`);
+    sendSearch(info.selectionText);
+  }
+});
+
+/*
+// Create one test item for each context type.
+var contexts = ["page","selection","link","editable","image","video",
+                "audio"];
+for (var i = 0; i < contexts.length; i++) {
+  var context = contexts[i];
+  var title = "Testar katter";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                       "onclick": genericOnClick});
+  console.log("'" + context + "' item:" + id);
+}
+*/
